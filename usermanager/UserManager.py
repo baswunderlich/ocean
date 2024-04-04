e408420ed5753079ccef95c5b2180caff4a929a7
baswunderlich <bastian.wunderlich@protonmail.com>
2024-04-04 08:42:51 +0200

    add hashed password storing

--- usermanager/UserManager.py
+++ usermanager/UserManager.py
@@ -1,6 +1,6 @@
 import secrets
-import json
 import string
+import hashlib
 
 def load_users() -> dict:
     #Here a file should be read to load existing users
@@ -18,10 +18,7 @@ class User:
 
     def __init__(self, name: str, password: str = ""):
         self.name = name
-        if password == "":
-            self.password = generate_password()
-        else:
-            self.password = password
+        self.password = str(hashlib.sha256(password.encode()).digest())
 
 def get_user(username: str) -> User:
     return users.get(username)
@@ -34,12 +31,13 @@ def username_exists(username: str) -> bool:
 def create_user(username: str) -> User:
     if(username_exists(username=username)):
         return None
-    new_user = User(name=username)
+    password = generate_password()
+    new_user = User(name=username, password=password)
     users[new_user.name] = new_user
-    return new_user
+    return (new_user, password)
 
 def is_password_correct(username: str, password: str) -> bool:
     user = users.get(username)
     if user is None:
         return False 
-    return user.password == password
+    return user.password == str(hashlib.sha256(password.encode()).digest())
